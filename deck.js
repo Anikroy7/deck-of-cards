b2cf65bfcebb37b07bc7e1fbd62abe2990c5cba4
Anikroy7 <roy645800@gmail.com>
2022-02-27 17:42:03 +0600

    input and button added

--- deck.js
+++ deck.js
@@ -1,1 +1,6 @@
 console.log('this is deck of card')
+document.getElementById('input-button').addEventListener('click', function () {
+    const inputId = document.getElementById('input-field');
+    const inputTextValue = inputId.value;
+    console.log(inputTextValue)
+})
